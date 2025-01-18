7c2a0949e1427098c2c26bd6c4f3018bf78f2911
Venkatesh Srinivas <venkatesh.srinivas1404@gmail.com>
2025-01-18 14:16:47 +0530

    save

--- task6.1/metrics.py
+++ task6.1/metrics.py
@@ -14,7 +14,7 @@ class_folders = [
 ]
 
 #output directory
-output_dir = "C:\\college_stuff\\events\\impulse\\task4.1\\output_plots"
+output_dir = "C:\\college_stuff\\events\\impulse\\task6.1\\metrics_output"
 os.makedirs(output_dir, exist_ok=True)
 
 #function to compute time domain metrics for EEG data
@@ -42,7 +42,7 @@ def compute_metrics(eeg_data):
 
 
 #function to save metrics
-def save_metrics(file_path, output_dir, class_folder):
+def save_metrics(file_path, output_dir):
     eeg_data = np.load(file_path)
 
     file_name = os.path.basename(file_path).split(".")[0]
@@ -55,7 +55,6 @@ def save_metrics(file_path, output_dir, class_folder):
     # Save metrics to a CSV file
     metrics_file_path = os.path.join(output_dir, f"{file_name}_metrics.csv")
     metrics_df.to_csv(metrics_file_path, index=False)
-    print(f"Metrics saved to: {metrics_file_path}")
 
 
 #processing the files in each class folder
@@ -67,16 +66,16 @@ for class_folder in class_folders:
 
     #list of .npy files
     npy_files = [f for f in os.listdir(class_path) if f.endswith(".npy")]
+    npy_files = sorted(npy_files)
+
+    # creating an output subdirectory for the class
+    class_output_dir = os.path.join(output_dir, class_folder)
+    os.makedirs(class_output_dir, exist_ok=True)
 
     #selecting the first file
     for selected_file in npy_files:
         file_path = os.path.join(class_path, selected_file)
-
-        #creating an output subdirectory for the class
-        class_output_dir = os.path.join(output_dir, class_folder)
-        os.makedirs(class_output_dir, exist_ok=True)
-
         #plotting the signals and computing metrics for the selected file
-        plot_signals(file_path, class_output_dir, class_folder)
+        save_metrics(file_path, class_output_dir)
 
 print("Plots and metrics have been generated and saved in the output directory.")
